90b237c18f95a9c7e07f08624121f35c3b7168d2
Ghufran <m_ghufran@mhs.unsyiah.ac.id>
2023-04-13 16:06:13 +0700

    Feat: add a new project laravel breeze

--- euran/resources/views/components/input.blade.php
+++
@@ -1,3 +0,0 @@
-@props(['disabled' => false])
-
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'text-black border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-2x shadow-sm']) !!}>
