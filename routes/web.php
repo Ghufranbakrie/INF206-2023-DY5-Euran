4c77ac7497740270026564a8420cdee617777ab5
rachmatfajar <rachmat3@mhs.unsyiah.ac.id>
2023-04-14 00:06:44 +0700

    update:routing

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/login', function () {
     return view('auth.login');
 });
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
