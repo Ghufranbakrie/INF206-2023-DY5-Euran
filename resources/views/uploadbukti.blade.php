738af1c0d21f51a8bc98964ecece6f44b8ae7e5f
putri sm <p_sakina@mhs.unsyiah.ac.id>
2023-05-10 22:45:48 +0700

    dosc: modify uploadbukti

--- resources/views/uploadbukti.blade.php
+++ resources/views/uploadbukti.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-URAN | List Rumah</title>
+    <title>E-URAN | Upload Bukti</title>
     <script src="https://cdn.tailwindcss.com"></script>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.css" rel="stylesheet" />
 </head>
